3336c699f31ec2ad49fb169d8d073443fc39f0aa
Brian Marco <brian@mademonkey.com>
2024-02-16 16:46:21 -0500

    support file for bernoulli test run

--- cmd.py
+++ cmd.py
@@ -3,7 +3,13 @@ import os
 from cmdstanpy import CmdStanModel
 
 stan_file = os.path.join('bernoulli.stan')
+data_file = os.path.join('bernoulli.data.json')
 
+# instantiate a model; compiles the Stan program by default
 model = CmdStanModel(stan_file=stan_file)
 
-print(model)
+# obtain a posterior sample from the model conditioned on the data
+fit = model.sample(chains=4, data=data_file)
+
+# summarize the results (wraps CmdStan `bin/stansummary`):
+fit.summary()
